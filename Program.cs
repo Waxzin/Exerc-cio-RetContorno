4449565f45058e22b7748ea09633b5b27f918a2e
matheus Luan <matheusluan04@gmail.com>
2026-05-22 22:40:06 -0300

    Inicio de atividade

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Tamanho do Largura:");
+int  largura = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Tamanho da Altura:");
+int  altura = Convert.ToInt32(Console.ReadLine());
+
+string preenchimento = "*";
+
+int contadorLargura = 0;
+while (contadorLargura < altura) 
+{
+    
+}
