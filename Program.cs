00795a7ffb2c77e60343d0a0e4e6e4457cbf8317
Matheus Luan <matheusluan04@gmail.com>
2026-05-29 22:17:21 -0300

    Finalizção de atividade

--- Program.cs
+++ Program.cs
@@ -1,13 +1,38 @@
-﻿Console.Write("Tamanho do Largura:");
+﻿
+string continuar ="s";
+
+
+while (continuar == "s"){
+
+Console.Write("Tamanho do Largura:");
 int  largura = Convert.ToInt32(Console.ReadLine());
 
 Console.Write("Tamanho da Altura:");
 int  altura = Convert.ToInt32(Console.ReadLine());
+;
 
-string preenchimento = "*";
-
-int contadorLargura = 0;
-while (contadorLargura < altura) 
+int contadorLinha = 0;
+while (contadorLinha < altura) 
 {
-    
+int contadorcoluna = 0;
+    while (contadorcoluna < largura)
+    {
+        if (contadorLinha == 0 || contadorLinha == altura - 1 || contadorcoluna == 0 || contadorcoluna == largura - 1)
+        {
+            Console.Write("*");
+        }
+        else
+        {
+            Console.Write(" "); 
+        }
+        
+        contadorcoluna = contadorcoluna + 1;
+    }
+    Console.WriteLine();
+    contadorLinha = contadorLinha + 1;
 }
+
+Console.Write("Deseja executar novamente? (S/N)?");
+continuar = Console.ReadLine()!.ToLower();
+}
+Console.WriteLine("muito obrigado por usar o programa!");
